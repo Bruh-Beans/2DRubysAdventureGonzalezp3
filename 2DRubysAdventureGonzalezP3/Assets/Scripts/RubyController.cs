89fcc032a9e654ee14b7c7a4d4809a97574b603e
Bruh-Beans <723536@student.dusd.net>
2023-10-27 11:39:49 -0700

    ADDED MOVEMENT
    Added movement up and down, as well as added the base for tilemaps;

--- 2DRubysAdventureGonzalezP3/Assets/Scripts/RubyController.cs
+++ 2DRubysAdventureGonzalezP3/Assets/Scripts/RubyController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class RubyController : MonoBehaviour
 {
@@ -13,8 +14,12 @@ public class RubyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
         transform.position = position;
     }
 }
