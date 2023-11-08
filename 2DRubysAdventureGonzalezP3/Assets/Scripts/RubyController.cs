4cbb6f3c8494e5f49c0fd8b18736639a29317290
Bruh-Beans <723536@student.dusd.net>
2023-11-08 11:42:49 -0800

    stuff

--- 2DRubysAdventureGonzalezP3/Assets/Scripts/RubyController.cs
+++ 2DRubysAdventureGonzalezP3/Assets/Scripts/RubyController.cs
@@ -7,7 +7,7 @@ public class RubyController : MonoBehaviour
 {
     public float speed = 3.0f;
     public int maxHealth = 5;
-    int currentHealth;
+    int currentHealth = 1;
 
     Rigidbody2D rigidbody2d;
     float horizontal;
@@ -35,9 +35,8 @@ public class RubyController : MonoBehaviour
 
         rigidbody2d.MovePosition(position);
     }
-    void ChangeHealth(int amount)
+    public void ChangeHealth(int amount)
     {
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
-}
