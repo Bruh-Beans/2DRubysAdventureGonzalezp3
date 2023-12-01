9c3d520a4991933ffdcea3e5303041643b626ff9
Bruh-Beans <723536@student.dusd.net>
2023-11-30 23:26:09 -0800

    Finised game
    game is finished but there is a error where rubys doesn't do an animation when taking damage.

---
+++ 2DRubysAdventureGonzalezP3/Assets/Scripts/NonPlayerCharacter.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+[DebuggerDisplay("{" + nameof(GetDebuggerDisplay) + "(),nq}")]
+public class NonPlayerCharacter : MonoBehaviour
+{
+    public float displayTime = 4.0f;
+    public GameObject dialogBox;
+    float timerDisplay;
+
+    void Start()
+    {
+        dialogBox.SetActive(false);
+        timerDisplay = -1.0f;
+    }
+
+    void Update()
+    {
+        if (timerDisplay >= 0)
+        {
+            timerDisplay -= Time.deltaTime;
+            if (timerDisplay < 0)
+            {
+                dialogBox.SetActive(false);
+            }
+        }
+    }
+
+    public void DisplayDialog()
+    {
+        timerDisplay = displayTime;
+        dialogBox.SetActive(true);
+    }
+
+    private string GetDebuggerDisplay()
+    {
+        return ToString();
+    }
+}
